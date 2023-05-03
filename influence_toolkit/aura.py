58ea10dde89d121544b80883a7bff2cc3320d61d
Petrovska <petrovska@badger.com>
2023-05-03 20:24:22 +0900

    feat: add another df for displaying general aura insights and include missing constants

--- influence_toolkit/aura.py
+++ influence_toolkit/aura.py
@@ -1,9 +1,16 @@
+from ape import Contract
+
+from influence_toolkit.constants import AURA
 from influence_toolkit.constants import AURA_FEE
 from influence_toolkit.constants import BALANCER_EMISSIONS
 from influence_toolkit.constants import POOL_ID_DIGG
+from influence_toolkit.constants import PROXY_AURA_VOTER
+from influence_toolkit.constants import VEBAL
+from influence_toolkit.constants import VLAURA
 
 
-def aura_mint_ratio(aura):
+def aura_mint_ratio():
+    aura = Contract(AURA)
     total_cliffs = aura.totalCliffs()
     cliff_reduction = aura.reductionPerCliff() / 1e18
     aura_total_supply = aura.totalSupply() / 1e18
@@ -20,6 +27,21 @@ def weekly_emissions_after_fee(aura_mint_ratio, bal_price, aura_price):
     )
     weekly_emissions_after_fee = weekly_emissions * (1 - AURA_FEE)
     return weekly_emissions_after_fee
+
+
+def vebal_controlled_per_aura():
+    # contracts
+    vebal = Contract(VEBAL)
+    vlAURA = Contract(VLAURA)
+
+    # calcs
+    voter_proxy_aura_vebal_vp = vebal.balanceOfAt(PROXY_AURA_VOTER) / 1e18
+
+    ts_vlaura = vlAURA.totalSupply() / 1e18
+
+    vebal_controlled_per_aura = voter_proxy_aura_vebal_vp / ts_vlaura
+
+    return vebal_controlled_per_aura
 
 
 def get_gravi_in_balancer_pool(balancer_vault):
--- influence_toolkit/df_helper.py
+++ influence_toolkit/df_helper.py
@@ -3,6 +3,10 @@ import pandas as pd
 from influence_toolkit.constants import POOL_INDEXES
 from influence_toolkit.pool_tvls import get_pool_tvls
 from influence_toolkit.treasury_captures import get_treasury_captures
+from influence_toolkit.aura import aura_mint_ratio
+from influence_toolkit.aura import vebal_controlled_per_aura
+from influence_toolkit.vp_info import get_council_vp_fee
+from influence_toolkit.vp_info import get_voter_vp
 
 
 def pct_format(figure):
@@ -13,13 +17,27 @@ def dollar_format(figure):
     return "${:,.2f}".format(figure)
 
 
-def display_df():
+def display_current_epoch_df():
     pools_tvl = [dollar_format(x) for x in get_pool_tvls()]
     # TODO: captures = [pct_format(x) for x in get_treasury_captures()]
     captures = [0.2, 0.3, 0.4, 0.6]
 
     df = {"Pools": POOL_INDEXES, "TVL": pools_tvl, "Capture": captures}
     df = pd.DataFrame(df)
-    df.set_index("Pools")
+
+    return df.set_index("Pools")
+
+
+def display_aura_df():
+    headers = ["Mint Ratio", "Treasury VP", "Council Fee VP", "veBAL per Aura"]
+
+    mint_ratio = aura_mint_ratio()
+    treasury_votes = get_voter_vp()
+    council_fee = get_council_vp_fee()
+    vebal_per_aura = vebal_controlled_per_aura()
+
+    data = [mint_ratio, treasury_votes, council_fee, vebal_per_aura]
+
+    df = pd.DataFrame(data, columns=headers)
 
     return df
