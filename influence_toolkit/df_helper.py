8808ebe2299d18f75c13963052d18cdbc01f9572
Petrovska <petrovska@badger.com>
2023-05-03 19:48:49 +0900

    feat: add structure to the display df helper

--- influence_toolkit/df_helper.py
+++ influence_toolkit/df_helper.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+from influence_toolkit.constants import POOL_INDEXES
+from influence_toolkit.pool_tvls import get_pool_tvls
+from influence_toolkit.treasury_captures import get_treasury_captures
+
 
 def pct_format(figure):
     return "{0:.1%}".format(figure)
@@ -9,8 +13,12 @@ def dollar_format(figure):
     return "${:,.2f}".format(figure)
 
 
-def display_df(labels, values):
-    data = [values]
-    headers = labels
-    df = pd.DataFrame(data, columns=headers)
+def display_df():
+    pools_tvl = get_pool_tvls()
+    captures = [0.2, 0.3, 0.4, 0.6]
+
+    df = {"Pools": POOL_INDEXES, "TVL": pools_tvl, "Capture": captures}
+    df = pd.DataFrame(df)
+    df.set_index("Pools")
+
     return df
