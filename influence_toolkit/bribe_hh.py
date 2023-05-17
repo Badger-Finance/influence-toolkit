933ccc7b44d763f58b3de7a6f46f17131cf7424a
Petrovska <petrovska@badger.com>
2023-05-16 20:50:55 -0600

    fix: sort out id and remove method from tvls py file

--- influence_toolkit/bribe_hh.py
+++ influence_toolkit/bribe_hh.py
@@ -6,7 +6,7 @@ from influence_toolkit.constants import LLAMA_DASHBOARD_URL
 def get_usd_vlaura_hh():
     r = requests.post(
         LLAMA_DASHBOARD_URL,
-        json={"platform": "hh", "protocol": "aura-bal"},
+        json={"id": "bribes-overview-aura"},
     ).json()
 
     dollar_per_lock_asset = r["dashboard"]["epochs"][-1]["dollarPerVlAsset"]
